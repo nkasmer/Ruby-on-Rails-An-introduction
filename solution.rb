d62e0fda92891e2b8553026cf4de8aaa3f2ec660
Nick Kasmeridis <nkasmer@users.noreply.github.com>
2016-05-06 00:15:01 +0300

    A script for testing your solution
    This script provides a sample execution of the Solution class that is consistent with what the rspec evaluation will perform. You may use it to sanity check your actual solution along side the rspec tests.

--- solution.rb
+++ solution.rb
@@ -3,7 +3,7 @@ require_relative "module2_assignment"
 # This script provides a sample execution of the Solution class
 # that is consistent with what the rspec evaluation will perform.
 # You may use it to sanity check your actual solution along side
-# the rspec tests.
+# the rspec tests. 
 
 solution = Solution.new
 #expect errors until you implement these methods
